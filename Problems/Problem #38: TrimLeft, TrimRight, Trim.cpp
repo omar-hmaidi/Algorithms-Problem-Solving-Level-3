dbd4ba2a872cccc5385ee790e79b10b748565ce4
Omar al-hmaidi <157281539+omar-hmaidi@users.noreply.github.com>
2024-09-19 13:09:28 +0300

    Create Problem #38: TrimLeft, TrimRight, Trim.cpp

---
+++ Problems/Problem #38: TrimLeft, TrimRight, Trim.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string ReadString()
+{
+	string S1;
+	cout << "Please enter your string?\n";
+	getline(cin, S1);
+	return S1;
+}
+
+string TrimLeft(string S1)
+{
+
+	for (short i = 0; i <= S1.length(); i++)
+	{
+		if (S1[i] != ' ')
+		{
+			return S1.substr(i, S1.length() - i);
+		}
+	}
+	return "";
+
+}
+
+string TrimRight(string S1)
+{
+
+	for (short i = S1.length() - 1; i >= 0; i--)
+	{
+		if (S1[i] != ' ')
+		{
+			return S1.substr(0, i + 1);
+		}
+	}
+	return "";
+}
+
+string Trim(string S1)
+{
+	return (TrimLeft(TrimRight(S1)));
+}
+
+
+
+int main()
+{
+	string S1 = "     OMAR AL-HMAIDI     ";
+
+	cout << "\nString     = " << S1;
+	cout << "\n\nTrim Left  = " << TrimLeft(S1);
+	cout << "\nTrim Right = " << TrimRight(S1);
+	cout << "\nTrim       = " << Trim(S1);
+
+	system("pause>0");
+
+	return 0;
+}
