eff566ed3ef4b4030227a2655beb9db1c34a4f7b
Omar al-hmaidi <157281539+omar-hmaidi@users.noreply.github.com>
2024-09-19 05:50:53 +0300

    Problem #35: Create Print Each Word In String.cpp

---
+++ Problems/Print Each Word In String.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string ReadString()
+{
+	string S1;
+	cout << "Please enter your string?\n";
+	getline(cin, S1);
+	return S1;
+}
+
+void PrintEachWordInString(string S1)
+{
+
+
+	string delim = " "; // delimiter
+
+	cout << "\nYour string words are: \n\n";
+	short pos = 0;
+	string sWord; // define a string variable
+
+	//use find() function to get the position of the delimiters
+	
+	while ((pos = S1.find(delim)) != std::string::npos)
+	{
+
+		sWord = S1.substr(0, pos); // store the word
+		if(sWord!="")
+		{
+			cout << sWord << endl;
+		}
+		S1.erase(0, pos + delim.length()); // erase() until position and move to next word.
+
+	}
+
+	if (S1 != "")
+	{
+		cout << S1 << endl; // it print last word of the string.
+	}
+
+}
+
+int main()
+{
+	PrintEachWordInString(ReadString());
+
+	system("pause>0");
+
+	return 0;
+}
